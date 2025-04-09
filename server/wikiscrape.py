7d0567075826edd8b2ab40b4a7505896fbfab1cd
Cameron Kramer-Brown <cameron.kramerbrown1@gmail.com>
2025-04-09 17:43:22 +0100

    updated wikiScrape to explain how to load up a whole page

--- server/wikiscrape.py
+++ server/wikiscrape.py
@@ -8,6 +8,8 @@ def wikiScrapeCategories(categoryName, resultCount):
     category = wiki.categorymembers(categoryName,results=resultCount, subcategories=False) #ISSUE: need to already know the total number of results, otherwise stops way too early.. set it to a super high number? will that work? idk #categoryName = name of category in url: 'English-only_movement' gets the url https://en.wikipedia.org/wiki/Category:English-only_movement.
 
     return category
-    for thing in category:
-        print(thing + '\n')
+    for page in category: ##to load a page into an object, this line below is how (vvvv)
+        wikipage = wiki.page(page)
+
+
 
